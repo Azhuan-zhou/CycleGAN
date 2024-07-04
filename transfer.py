398c99f41553c6a65a9a90285d31f825725be96d
Azhuan-zhou <137917749+Azhuan-zhou@users.noreply.github.com>
2024-07-04 16:43:53 -0700

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -3,7 +3,8 @@ from models import create_model
 from PIL import Image
 import torchvision.transforms as transforms
 import torch
-import numpy as np
+import argparse
+import os
 from util import util
 # 定义图像转换操作
 transform = transforms.Compose([
@@ -11,6 +12,40 @@ transform = transforms.Compose([
     transforms.ToTensor(),  # 将图像转换为 Tensor
 ])
 
+
+def __patch_instance_norm_state_dict(state_dict, module, keys, i=0):
+    """Fix InstanceNorm checkpoints incompatibility (prior to 0.4)"""
+    key = keys[i]
+    if i + 1 == len(keys):  # at the end, pointing to a parameter/buffer
+        if module.__class__.__name__.startswith('InstanceNorm') and \
+                (key == 'running_mean' or key == 'running_var'):
+            if getattr(module, key) is None:
+                state_dict.pop('.'.join(keys))
+        if module.__class__.__name__.startswith('InstanceNorm') and \
+                (key == 'num_batches_tracked'):
+            state_dict.pop('.'.join(keys))
+    else:
+        __patch_instance_norm_state_dict(state_dict, getattr(module, key), keys, i + 1)
+
+def load_networks(model,path):
+    """Load all the networks from the disk.
+
+    Parameters:
+        epoch (int) -- current epoch; used in the file name '%s_net_%s.pth' % (epoch, name)
+    """
+    print('loading the model from %s' % path)
+    net = model.netG
+    if isinstance(net, torch.nn.DataParallel):
+        net = net.module
+    # if you are using PyTorch newer than 0.4 (e.g., built from
+    # GitHub source), you can remove str() on self.device
+    state_dict = torch.load(path, map_location=str(model.device))
+    if hasattr(state_dict, '_metadata'):
+        del state_dict._metadata
+    # patch InstanceNorm checkpoints prior to 0.4
+    for key in list(state_dict.keys()):  # need to copy keys here because we mutate in loop
+        __patch_instance_norm_state_dict(state_dict, net, key.split('.'))
+    net.load_state_dict(state_dict)
 
 def prepare_data(pathB):
     data = {}
@@ -28,12 +63,10 @@ def prepare_data(pathB):
     return data
 
 
-def init_model(opt):
+def init_model(opt,path):
     model = create_model(opt)  # create a model given opt.model and other options
-    model.setup(opt)
-    #path = './checkpoints/{}/latest_net_G.pth'.format(opt.name)
-    #checkpoint = torch.load(path)
-    #model.netG.load_state_dict(checkpoint)
+    load_networks(model,path)
+
     return model
 
 
@@ -45,17 +78,18 @@ def generate(model, data):
 
 
 if __name__ == '__main__':
-    A_paths = ['./Azhuan/A/4.png']
-    B_paths = ['./Azhuan/B/4.png']
+
+    A_paths = os.listdir('./Azhuan/A/')
+    B_paths = ['./Azhuan/B/{}'.format(path) for path in A_paths]
+    A_paths = ['./Azhuan/A/{}'.format(path) for path in A_paths]
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test --model test
-    opt.no_dropout = True
-    opt.name = 'style_vangogh_pretrained'
+    path = './checkpoints/{}/{}'.format(opt.model_name, opt.checkpoint_name)
     opt.num_threads = 0  # test code only supports num_threads = 0
     opt.no_flip = True  # no flip; comment this line if Azhuan on flipped images are needed.
     opt.display_id = -1  # no visdom display; the test code saves the Azhuan to a HTML file.
     data = prepare_data(A_paths)
-    model = init_model(opt)
+    model = init_model(opt,path)
     results = generate(model,data)['fake']
     l = len(A_paths)
     for i in range(l):
